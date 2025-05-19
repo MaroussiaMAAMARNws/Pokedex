bca2e5729ee1c18a687fb044e25c65ebd39e328e
Maroussia <mmaamar@normandiewebschool.fr>
2025-05-19 14:00:08 +0200

    js

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 async function Pokedex() {
-    const response = await fetch ("https://pokeapi.co");
-    const pokemon = await response.json();
-    console.log (pokemon);
+    try {
+        const response = await fetch ("https://pokeapi.co/api/v2/pokemon/20");
+        if (!reponse.ok) throw new Error("Erreur HTTP : " + reponse.status);
+        const data = await reponse.json();
+        console.log(data.name);
+    }
+    catch (err) {
+        console.error("Problème lors du chargement :", err.message);
+        }
 }
